d6ea0cef4e38c261f85c458a842d681f4c9196a7
hanna0616 <64650462+hanna0616@users.noreply.github.com>
2024-01-21 21:29:02 +0100

    Create app.js

---
+++ apps/HeartTrackerStressless/app.js
@@ -0,0 +1,23 @@
+
+var  on = false;
+E.showMessage("recording heart rate");
+setInterval(function() {
+  on = !on;
+  LED2.write(on);
+}, 500);
+
+Bangle.setHRMPower(1);
+
+var heartData = require("Storage").open("stresslessheartlog.csv","a");
+
+Bangle.on('HRM',function(hrm) {
+    var d = [
+      "H",
+       0|getTime(), // Time to the nearest second
+      hrm.bpm,
+      hrm.confidence
+      ];
+   // Write data here
+  heartData.write(d.join(",")+"\n");
+  Bluetooth.println(d.join(","));
+});
