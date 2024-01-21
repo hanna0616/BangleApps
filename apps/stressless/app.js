8b34de661f4d34702d001771361d017ccbea143f
hanna0616 <64650462+hanna0616@users.noreply.github.com>
2024-01-21 22:25:00 +0100

    Update app.js

--- apps/stressless/app.js
+++ apps/stressless/app.js
@@ -11,29 +11,6 @@ Bangle.setHRMPower(1);
 var heartData = require("Storage").open("stresslessheartratelog.csv","a");
 
 
-
-/*
-function timeConverter(UNIX_timestamp){
-// Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds
-var date = new Date(UNIX_timestamp * 1000);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
-  var year = date.getFullYear();
-  var month = months[date.getMonth()];
-  var day = date.getDate();
-  
-// Hours part from the timestamp
-var hours = date.getHours();
-
-// Minutes part from the timestamp
-var minutes = "0" + date.getMinutes();
-
-// Seconds part from the timestamp
-var seconds = "0" + date.getSeconds();
-
-var formattedTime = year + month+ day + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-}*/
 Bangle.on('HRM',function(hrm) {
     var d = [
       "HeartRate",
